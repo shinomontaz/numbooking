32ef82ac62dcbb7a6ff31dd6b265d462909f8627
shinomontaz@gmail.com <shinomontaz@gmail.com>
2020-10-23 18:31:23 +0300

    fix for wg.Wait

--- kafka-sourcer/main.go
+++ kafka-sourcer/main.go
@@ -5,7 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"sync"
+	"time"
 
 	"numbooking/kafka-sourcer/config"
 
@@ -25,6 +27,7 @@ func init() {
 
 	errors = make(chan error, 1000)
 
+	rand.Seed(time.Now().UnixNano())
 }
 
 func main() {
@@ -44,8 +47,10 @@ func main() {
 	go getDeletedNums(chMessages, chOk)
 
 	go func() {
-		<-chOk
-		wg.Done()
+		for range chOk {
+			fmt.Println("Done")
+			wg.Done()
+		}
 	}()
 
 	go func() {
@@ -58,26 +63,28 @@ func main() {
 				errors <- err
 			}
 		}
+
+		return
 	}()
 
 	wg.Wait()
+
 	close(chOk)
 	close(chMessages)
 }
 
 func getCreatedNums(chMessages chan<- []byte, chOk chan<- struct{}) {
-	sql := `SELECT fiRec_hist_rec as fidid_serial
-	FROM rec_history
-   WHERE firec_hist_tab = (
-		 SELECT fitable_id FROM tables_tq 
-		  WHERE fstable_name = 'did_numbers') -- по таблице DID
-	 AND farec_hist_dttm > %d -- Старше сегодняшнего дня
-	 AND firec_hist_func = 1 -- Создание
-  `
+	defer func() { chOk <- struct{}{} }()
+	// 	sql := `SELECT fiRec_hist_rec as fidid_serial
+	// 	FROM rec_history
+	//    WHERE firec_hist_tab = (
+	// 		 SELECT fitable_id FROM tables_tq
+	// 		  WHERE fstable_name = 'did_numbers') -- по таблице DID
+	// 	 AND farec_hist_dttm > %d -- Старше сегодняшнего дня
+	// 	 AND firec_hist_func = 1 -- Создание
+	//   `
 
-	sql = fmt.Sprintf(sql, 1)
-
-	chOk <- struct{}{}
+	// 	sql = fmt.Sprintf(sql, 1)
 }
 
 func getDeletedNums(chMessages chan<- []byte, chOk chan<- struct{}) {
@@ -93,12 +100,27 @@ func getDeletedNums(chMessages chan<- []byte, chOk chan<- struct{}) {
 	//   `
 	//   sql = fmt.Sprintf(sql)
 
-	num := types.DidNumber{}
+	var rndID int64
+	var rndDelFlag int
+	for i := 0; i < 100; i++ {
+		fmt.Println(i)
+		rndID = rand.Int63()
+		rndDelFlag = rand.Intn(2)
+		var delFlag bool
+		if rndDelFlag > 0 {
+			delFlag = true
+		}
 
-	jsonNum, err := json.Marshal(num)
-	if err != nil {
-		log.Fatal(err)
+		num := types.DidNumber{
+			ID:        rndID,
+			IsDeleted: delFlag,
+		}
+
+		jsonNum, err := json.Marshal(num)
+		if err != nil {
+			fmt.Println(err)
+			log.Fatal(err)
+		}
+		chMessages <- jsonNum
 	}
-	chMessages <- jsonNum
-
 }
